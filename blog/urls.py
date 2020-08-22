04debd3076f5a7e7433ef02ac9c19dff906a75bc
ning2510 <1367560112@qq.com>
2020-08-22 23:54:24 +0800

    class based view

--- blog/urls.py
+++ blog/urls.py
@@ -5,13 +5,13 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     # django 会从用户访问的 URL 中自动提取 URL 路径参数转换器 <type:name> 规则捕获的值，然后传递给其对应的视图函数。
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # 文章页面
-    path('posts/<int:pk>/', views.detail, name='detail'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
     # 归档页面
-    path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
+    path('archives/<int:year>/<int:month>/', views.ArchiveView.as_view(), name='archive'),
     # 分类页面
-    path('categories/<int:pk>/', views.category, name='category'),
+    path('categories/<int:pk>/', views.CategoryView.as_view(), name='category'),
     # 标签页面
-    path('tags/<int:pk>/', views.tag, name='tag'),
+    path('tags/<int:pk>/', views.TagView.as_view(), name='tag'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,49 +5,68 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Category, Tag
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
+from django.views.generic import DetailView, ListView
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
 
-def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class CategoryView(IndexView):
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super().get_queryset().filter(category=cate)
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+class ArchiveView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super().get_queryset().filter(created_time__year=year,
+                                             created_time__month=month)
 
-    # 阅读量 +1
-    post.increase_views()
 
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        # 记得在顶部引入 TocExtension 和 slugify
-        TocExtension(slugify=slugify),
-    ])
-    post.body = md.convert(post.body)
-    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-    post.toc = m.group(1) if m is not None else ''
-    return render(request, 'blog/detail.html', context={'post': post})
+class TagView(IndexView):
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super().get_queryset().filter(tags=t)
 
-# 归档页面
-def archive(request, year, month):
-    # 这里 created_time 是 Python 的 date 对象, 其有一个 year 和 month 属性
-    # Python 中调用属性的方式通常是 created_time.year，但是由于这里作为方法的参数列表，所以 django 要求我们把点替换成了两个下划线，即 created_time__year
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month
-                                    ).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
 
-# 分类页面
-# 根据 pk(被访问的分类的id值) 从数据库获取这个分类
-def category(request, pk):
-    # get_object_or_404:如果用户访问的分类不存在，则返回一个 404 错误页面以提示用户访问的资源不存在。
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+# 记得在顶部导入 DetailView
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
-def tag(request, pk):
-    # 记得在开始部分导入 Tag 类
-    t = get_object_or_404(Tag, pk=pk)
-    post_list = Post.objects.filter(tags=t).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super().get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super().get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1) if m is not None else ''
+
+        return post
