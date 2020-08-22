bf6d1443d60c2bf581e5e3b17e29e6614c314ca2
ning2510 <1367560112@qq.com>
2020-08-22 17:21:42 +0800

    modified blogproject

---
+++ blogproject/settings/local.py
@@ -0,0 +1,9 @@
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'development-secret-key'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
---
+++ blogproject/settings/production.py
@@ -0,0 +1,9 @@
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['47.92.151.126', 'liang-sheng.site']
